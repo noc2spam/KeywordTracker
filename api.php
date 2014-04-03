1f0d63d3bc5a9e74d50c8eac0db5c83cf98417b1
noc2spam <thcdesigning@gmail.com>
2014-04-03 16:10:14 +0530

    code cleanup

--- api.php
+++ api.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 
 This script is licensed under Apache 2.0. View the license here:
@@ -6,29 +7,17 @@ http://www.apache.org/licenses/LICENSE-2.0.html
 Copyright Reserved to g0g0l
 Contact @ Skype : noc2spam
 */
-
-/*$mtime = microtime();
-   $mtime = explode(" ",$mtime);
-   $mtime = $mtime[1] + $mtime[0];
-   $starttime = $mtime; 
-require 'bootstrap.php';
+require('bootstrap.php');
 
 //$user = new User();
 //$user->insert();
 
-  //  $user->attributes = array('a1' => rand(0, 4), 'a2' => rand(0, 44), 'a5' => rand(0, 444));
+//  $user->attributes = array('a1' => rand(0, 4), 'a2' => rand(0, 44), 'a5' => rand(0, 444));
 
 
 //$user->insert();
 
-var_dump($googleScraper);
-$mtime = microtime();
-   $mtime = explode(" ",$mtime);
-   $mtime = $mtime[1] + $mtime[0];
-   $endtime = $mtime;
-   $totaltime = ($endtime - $starttime);
-   echo "This page was created in ".$totaltime." seconds"; */
-   require('bootstrap.php');
+
 	$task = @$_REQUEST['task'];
 	
 	switch($task){
